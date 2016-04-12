455989bae1ddd5d711bebf27e47bba938084efad
ArronYR <yangyun4814@gmail.com>
2016-04-12 19:02:09 +0800

    初步完成基本样式

---
+++ SlideMenuQQ50/app/src/main/java/com/helloarron/slidemenuqq50/view/SlideMenu.java
@@ -0,0 +1,109 @@
+package com.helloarron.slidemenuqq50.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by arron on 16/4/12.
+ */
+public class SlideMenu extends HorizontalScrollView {
+
+    private LinearLayout mWrapper;
+    private ViewGroup mMenu;
+    private ViewGroup mContent;
+    private int mScreenWidth;
+    private int mMenuWidth;
+
+    // dp
+    private int mMenuRightPadding = 50;
+
+    private boolean once = false;
+
+    public SlideMenu(Context context) {
+        super(context);
+    }
+
+    /**
+     * 未使用自定义属性时，调用
+     * @param context
+     * @param attrs
+     */
+    public SlideMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        mScreenWidth = outMetrics.widthPixels;
+
+        // dp => px
+        mMenuRightPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * 设置子View的宽和高
+     * 设置自己的宽和高
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (!once){
+            mWrapper = (LinearLayout) getChildAt(0);
+            mMenu = (ViewGroup) mWrapper.getChildAt(0);
+            mContent = (ViewGroup) mWrapper.getChildAt(1);
+
+            mMenuWidth = mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
+            mContent.getLayoutParams().width = mScreenWidth;
+
+            once = true;
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * 通过设置偏移量，将menu隐藏
+     * @param changed
+     * @param l
+     * @param t
+     * @param r
+     * @param b
+     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+
+        if (changed){
+            this.scrollTo(mMenuWidth, 0);
+        }
+    }
+
+    /**
+     * touch事件
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_UP:
+                int scrollX = getScrollX();
+                if (scrollX >= mMenuWidth/2){
+                    this.smoothScrollTo(mMenuWidth, 0);
+                }else {
+                    this.smoothScrollTo(0,0);
+                }
+                return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+}
