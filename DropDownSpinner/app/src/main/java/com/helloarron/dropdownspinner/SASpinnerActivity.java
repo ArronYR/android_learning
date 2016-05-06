5c24dff600c8f7b0f23fa5aba33d899035f2704d
ArronYR <yangyun4814@gmail.com>
2016-05-06 19:24:57 +0800

    add layout files

--- DropDownSpinner/app/src/main/java/com/helloarron/dropdownspinner/SASpinnerActivity.java
+++ DropDownSpinner/app/src/main/java/com/helloarron/dropdownspinner/SASpinnerActivity.java
@@ -12,5 +12,6 @@ public class SASpinnerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sasa);
     }
 }
