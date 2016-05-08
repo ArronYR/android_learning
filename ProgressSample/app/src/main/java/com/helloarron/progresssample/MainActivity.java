293b20d694abd497ec3749d0e7c33cb4af026e14
ArronYR <yangyun4814@gmail.com>
2016-05-08 21:32:38 +0800

    create project

---
+++ ProgressSample/app/src/main/java/com/helloarron/progresssample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.helloarron.progresssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSysNormal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSysNormal = (Button) findViewById(R.id.sys_normal_btn);
+        btnSysNormal.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sys_normal_btn:
+                break;
+            default:
+                break;
+        }
+    }
+}
