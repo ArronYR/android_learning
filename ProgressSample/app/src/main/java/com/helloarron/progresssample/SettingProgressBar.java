747b8a9ce2ede9aa57e07a939f99a482eb280b09
ArronYR <yangyun4814@gmail.com>
2016-05-09 20:14:26 +0800

    modify setting activity

--- ProgressSample/app/src/main/java/com/helloarron/progresssample/SettingProgressBar.java
+++ ProgressSample/app/src/main/java/com/helloarron/progresssample/SettingProgressBar.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by arron on 16/5/9.
@@ -35,6 +38,7 @@ public class SettingProgressBar extends AppCompatActivity implements View.OnClic
 
     private ProgressBar progressBar;
     private Button btnAdd, btnReduce, btnReset;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,6 +46,8 @@ public class SettingProgressBar extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_setting);
 
         progressBar = (ProgressBar) findViewById(R.id.pb_horizontal);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+
         btnAdd = (Button) findViewById(R.id.add);
         btnReduce = (Button) findViewById(R.id.reduce);
         btnReset = (Button) findViewById(R.id.reset);
@@ -49,17 +55,32 @@ public class SettingProgressBar extends AppCompatActivity implements View.OnClic
         btnAdd.setOnClickListener(this);
         btnReduce.setOnClickListener(this);
         btnReset.setOnClickListener(this);
+
+        int first = progressBar.getProgress();
+        int second = progressBar.getSecondaryProgress();
+        int max = progressBar.getMax();
+
+        tvResult.setText("第一进度百分比: " + (int) (first / (float) max * 100) + "% ,第二进度百分比: " + (int) (second / (float) max * 100) + "%");
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.add:
+            case R.id.add: {
+                progressBar.incrementProgressBy(10);
+                progressBar.incrementSecondaryProgressBy(10);
                 break;
-            case R.id.reduce:
+            }
+            case R.id.reduce: {
+                progressBar.incrementProgressBy(-10);
+                progressBar.incrementSecondaryProgressBy(-10);
                 break;
-            case R.id.reset:
+            }
+            case R.id.reset: {
+                progressBar.setProgress(50);
+                progressBar.setSecondaryProgress(80);
                 break;
+            }
             default:
                 break;
         }
