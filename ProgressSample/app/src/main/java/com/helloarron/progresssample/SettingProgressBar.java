7a9154c51d57f5cbbf3b137a6898dc399ed27dc8
ArronYR <yangyun4814@gmail.com>
2016-05-09 20:00:16 +0800

    modify setting activity

--- ProgressSample/app/src/main/java/com/helloarron/progresssample/SettingProgressBar.java
+++ ProgressSample/app/src/main/java/com/helloarron/progresssample/SettingProgressBar.java
@@ -3,12 +3,14 @@ package com.helloarron.progresssample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 /**
  * Created by arron on 16/5/9.
  */
-public class SettingProgressBar extends AppCompatActivity {
+public class SettingProgressBar extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * 关键属性:
@@ -21,7 +23,7 @@ public class SettingProgressBar extends AppCompatActivity {
 
     /**
      * 关键方法：
-     *
+     * <p/>
      * setProgress(int);                    设置第一进度
      * setSecondaryProgress(int);           设置第二进度
      * getProgress();                       获取第一进度
@@ -32,6 +34,7 @@ public class SettingProgressBar extends AppCompatActivity {
      */
 
     private ProgressBar progressBar;
+    private Button btnAdd, btnReduce, btnReset;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,5 +42,26 @@ public class SettingProgressBar extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         progressBar = (ProgressBar) findViewById(R.id.pb_horizontal);
+        btnAdd = (Button) findViewById(R.id.add);
+        btnReduce = (Button) findViewById(R.id.reduce);
+        btnReset = (Button) findViewById(R.id.reset);
+
+        btnAdd.setOnClickListener(this);
+        btnReduce.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add:
+                break;
+            case R.id.reduce:
+                break;
+            case R.id.reset:
+                break;
+            default:
+                break;
+        }
     }
 }
