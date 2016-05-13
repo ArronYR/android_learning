6d7bc0db7c758b72feb5f6bab1ee7420e5deae12
ArronYR <yangyun4814@gmail.com>
2016-05-13 11:36:57 +0800

    create the WebViewSample project

---
+++ WebViewSample/app/src/main/java/com/helloarron/webviewsample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.helloarron.webviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnOpenInBrowser, btnOpenInWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenInBrowser = (Button) findViewById(R.id.open_in_browser_btn);
+        btnOpenInWebView = (Button) findViewById(R.id.open_in_webview_btn);
+
+        btnOpenInBrowser.setOnClickListener(this);
+        btnOpenInWebView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.open_in_browser_btn:
+                break;
+            case R.id.open_in_webview_btn:
+                break;
+            default:
+                break;
+        }
+    }
+}
