6553df467ffec4dd9a56297b75d7be09f1a06005
ArronYR <yangyun4814@gmail.com>
2016-05-17 11:52:13 +0800

    create this project & init MainActivity

---
+++ FragmentSample/app/src/main/java/com/helloarron/fragmentsample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.helloarron.fragmentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.first:
+                break;
+            case R.id.second:
+                break;
+            case R.id.third:
+                break;
+            case R.id.fourth:
+                break;
+            default:
+                break;
+        }
+    }
+}
