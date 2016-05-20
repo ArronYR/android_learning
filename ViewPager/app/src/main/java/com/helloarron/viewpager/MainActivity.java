9a42e4c2f4a26848cc8499a11e30216f7ced95f1
ArronYR <yangyun4814@gmail.com>
2016-05-20 11:54:53 +0800

    create ViewPager sample project

---
+++ ViewPager/app/src/main/java/com/helloarron/viewpager/MainActivity.java
@@ -0,0 +1,21 @@
+package com.helloarron.viewpager;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 初始化ViewPager
+        pager = (ViewPager) findViewById(R.id.pager);
+
+        
+    }
+}
