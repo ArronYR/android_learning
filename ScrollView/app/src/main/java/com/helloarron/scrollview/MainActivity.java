6e33d742785fdac386d9d0e9e9c3d2d89e2a5fce
ArronYR <yangyun4814@gmail.com>
2016-05-24 12:50:23 +0800

    completed listener

--- ScrollView/app/src/main/java/com/helloarron/scrollview/MainActivity.java
+++ ScrollView/app/src/main/java/com/helloarron/scrollview/MainActivity.java
@@ -2,13 +2,14 @@ package com.helloarron.scrollview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnUp, btnDown;
     private TextView content;
@@ -28,8 +29,41 @@ public class MainActivity extends AppCompatActivity {
         scroll.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE: {
+                        /**
+                         * getScrollY() -- 滚动条滑动的距离
+                         * getMeasuredHeight() -- 获取控件的总高度
+                         * getHeight() -- 获取控件在屏幕中的高度
+                         */
+
+                        // 顶部状态
+                        if (scroll.getScrollY() <= 0) {
+                            Log.i("Main", "顶部");
+                        }
+
+                        // 底部状态
+                        if (scroll.getChildAt(0).getMeasuredHeight() <= scroll.getScrollY() + scroll.getHeight()) {
+                            Log.i("Main", "滑动到底部");
+                            content.append(getResources().getString(R.string.content));
+                        }
+                        break;
+                    }
+                }
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.up:
+                break;
+            case R.id.down:
+                break;
+            default:
+                break;
+        }
     }
 }
