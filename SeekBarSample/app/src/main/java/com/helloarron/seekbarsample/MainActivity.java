365de385e9fe10f8aa74f5a5c2217a39535310d3
ArronYR <yangyun4814@gmail.com>
2016-05-27 10:40:07 +0800

    init project

--- SeekBarSample/app/src/main/java/com/helloarron/seekbarsample/MainActivity.java
+++ SeekBarSample/app/src/main/java/com/helloarron/seekbarsample/MainActivity.java
@@ -2,12 +2,56 @@ package com.helloarron.seekbarsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar seekBar;
+    private TextView tvStatus, tvValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvStatus = (TextView) findViewById(R.id.tv_status);
+        tvValue = (TextView) findViewById(R.id.tv_value);
+
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(this);
+    }
+
+    /**
+     * 数值改变时
+     *
+     * @param seekBar
+     * @param progress
+     * @param fromUser
+     */
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        tvStatus.setText("正在拖动");
+        tvValue.setText("当前数值: " + progress);
+    }
+
+    /**
+     * 开始拖动时
+     *
+     * @param seekBar
+     */
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        tvStatus.setText("开始拖动");
+    }
+
+    /**
+     * 拖动结束时
+     *
+     * @param seekBar
+     */
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        tvStatus.setText("停止拖动");
     }
 }
